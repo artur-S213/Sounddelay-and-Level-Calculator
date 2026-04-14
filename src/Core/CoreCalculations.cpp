250762cac0c561d1ce57ba69c83be7dd568857bc
Artur <artur@fedora.fritz.box>
2026-04-14 18:54:59 +0200

    Added cmath to core

--- src/Core/CoreCalculations.cpp
+++ src/Core/CoreCalculations.cpp
@@ -1,17 +1,21 @@
 #include "CoreCalculations.h"
 #include <math.h>
+#include <cmath>
 
-AcousticCalcResults CoreCalculations::calculateAllValues(float distance, float soundLevel){
+AcousticCalcResults CoreCalculations::calculateAllValues(float distance, float soundLevel)
+{
     AcousticCalcResults res;
 
     // Delay = distance/343 *1000
     res.delay = (distance / 343) * 1000;
 
-    //soundLevelL2 =  soundlevelL1 - 20 * LOG_10(distance)
-    if (distance <= 0.0){
+    // soundLevelL2 =  soundlevelL1 - 20 * LOG_10(distance)
+    if (distance <= 0.0)
+    {
         res.soundLevelL2 = 0.0;
     }
-    else {
+    else
+    {
         float calL2 = soundLevel - (20 * (std::log10(distance)));
         res.soundLevelL2 = (calL2 < 0.0) ? 0 : calL2;
     }
@@ -21,7 +25,7 @@ AcousticCalcResults CoreCalculations::calculateAllValues(float distance, float s
 
     // =WENN(droplevel<11;0; WENN(droplevel>11;droplevel-11))
     //  => Wenn der Abfallpegel kleiner 11 -> 0 sonst Abfallpegel - 11
-    res.adjustmentLevel = (res.dropLevel < ((float) 11)) ? 0 : res.dropLevel - ((float) 11);
+    res.adjustmentLevel = (res.dropLevel < ((float)11)) ? 0 : res.dropLevel - ((float)11);
 
     // =soundLevelL1 - Einstellspegel
     res.setpoint = soundLevel - res.adjustmentLevel;
